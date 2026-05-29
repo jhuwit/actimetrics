06725b656ca406d77f943f44ef4aeef5f5610712
muschellij2 <muschellij2@gmail.com>
2026-05-29 15:53:33 -0400

    no more pipe - just import

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,10 @@
-#' @importFrom actibase set_transformations get_transformations is.AccData
-#' @importFrom actibase acti_fill_zeros acti_resample acti_standardize_data
-#' @importFrom actibase get_sample_rate get_dynamic_range flag_qc_all
-#' @importFrom actiread acti_read_gt3x acti_read_cwa
+#' @import actibase
+#' @import actiread
+#' @importFrom magrittr `%>%`
 #' @import stats
 NULL
+
+
+# @importFrom actibase set_transformations get_transformations is.AccData as_datetime_safe
+# @importFrom actibase acti_fill_zeros acti_resample acti_standardize_data
+# @importFrom actibase get_sample_rate get_dynamic_range flag_qc_all
